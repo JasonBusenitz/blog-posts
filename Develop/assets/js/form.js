cd3a7c5a12468a87989462c8471e23d4f567e59d
unknown <jbus731@gmail.com>
2024-07-22 05:00:26 -0500

    Added working JS to form.js

--- Develop/assets/js/form.js
+++ Develop/assets/js/form.js
@@ -1,5 +1,32 @@
 // TODO: Create a variable that selects the form element
+const username = document.querySelector('#username');
+const title = document.querySelector('#title');
+const content = document.querySelector('#content');
+const submitBttn = document.querySelector('#submitBttn');
+const error = document.querySelector('#error');
 
-// TODO: Create a function that handles the form submission. Grab the form data and store it in local storage, then redirect to the blog page using the `redirectPage` function. If the form is submitted with missing data, display an error message to the user.
+// TODO: If the form is submitted with missing data, display an error message to the user.
+submitBttn.addEventListener('click', function (event) {
+    event.preventDefault();
 
-// TODO: Add an event listener to the form on submit. Call the function to handle the form submission.
+    userPost = {
+        username: username.value,
+        title: title.value,
+        content: content.value.trim()
+    };
+    console.log(userPost)
+
+    if (!userPost.username ||
+        !userPost.title ||
+        !userPost.content
+    ) {
+        error.textContent = `Please complete the form.`;
+    } else {
+
+        localStorage.setItem('userPost', JSON.stringify(userPost));
+
+        let url = './blog.html'
+        redirectPage(url);
+    };
+
+});
